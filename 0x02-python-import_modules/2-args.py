41f7b152f26345e53f6e133294c7283570fc8238
Ossai6 <calebossaiakpe@gmail.com>
2023-03-13 00:21:25 -0700

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import sys
-
-count_result = int(len(sys.argv)) - 1
-if count_result == 0:
-    print("0 arguments.")
-elif count_result == 1:     
-    print("1 arguments:")
-else:
-    print("{} arguments".format(count_result))
-for i in range(count_result):
-    print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == '__main__':
+    import sys
+    count_result = int(len(sys.argv)) - 1
+    if count_result == 0:
+        print("0 arguments.")
+    elif count_result == 1:     
+        print("1 arguments:")
+    else:
+        print("{} arguments".format(count_result))
+    for i in range(count_result):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
