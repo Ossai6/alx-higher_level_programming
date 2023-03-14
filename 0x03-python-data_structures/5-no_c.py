cd6667020dbea073ba6a7b2817336e71beebc9eb
Ossai6 <calebossaiakpe@gmail.com>
2023-03-13 19:27:22 -0700

    I made pycodestyle changes to 5-no_c.py file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 """ a function that removes all characters c and C from a string.   By Caleb
-    
+
     arg: the string
 
     return: a modified string without 'c' or 'C'
 """
+
 
 def no_c(my_string):
     string = ""
