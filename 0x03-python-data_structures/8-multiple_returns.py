faf76edb8a86b2027bdb091ddf68a4ca88054e82
Ossai6 <calebossaiakpe@gmail.com>
2023-03-14 09:06:32 -0700

    added files

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
     len_sentence = len(sentence)
 
     if len_sentence == 0:
-        result = (0, )
-    else:
-        _result = (len_sentence, sentence[0:1])
-        return _result
+        result = (0, None)
+    
+    _result = (len_sentence, sentence[0])
+    return _result
