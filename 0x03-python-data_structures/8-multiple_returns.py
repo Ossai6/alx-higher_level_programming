d557bbce7ee28668a7482409ab05cd4fc767fc22
Ossai6 <calebossaiakpe@gmail.com>
2023-03-14 08:59:40 -0700

    updated 8-multiple_returns.py file. i sliced the sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,6 +5,6 @@ def multiple_returns(sentence):
 
     if len_sentence == 0:
         result = (0, None)
-    else: 
-        _result = (len_sentence, sentence[:1])
-        return _result
+ 
+    _result = (len_sentence, sentence[0:1])
+    return _result
