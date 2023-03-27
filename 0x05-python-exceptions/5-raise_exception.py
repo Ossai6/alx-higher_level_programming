648c2eb2cd8f2684414c4fd2180e50229a0ab969
Ossai <calebossaiakpe@gmail.com>
2023-03-27 14:15:44 -0700

    added two more files

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" a function that raises an exception
+
+    args: 
+
+    returns: Exception
+
+    by Akpe Caleb
+
+"""
+ 
+
+def raise_exception():
+    raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" a function that raises an exception with a message
+
+    args: 
+
+    returns: a message
+
+    by Akpe Caleb
+
+"""
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
