681b98e8d2cf625849dde33c7e6802a5d6d0cd24
Ossai <calebossaiakpe@gmail.com>
2023-04-02 04:50:18 -0700

    advance

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+Defines a class Square
+"""
+
+
+class Square:
+    """ A class named Square
+    Attributes:
+        __size(int): The size of a side of the square
+    """
+    def __init__(self, size=0):
+        """This method initializes a square
+        Args:
+            size(int = 0): The size of a side of the square
+        Returns: None.
+        """
+        self.__size = size
+
+    def area(self):
+        """This method computea the area of a square
+        Args:
+            self: The object
+        Returns: The area of a square
+        """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """This method returns the size of a square
+        Args:
+            self: The object
+        Returns: The size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """This method validates the value of size of a square
+        Args:
+            value(int): The size of a side of the square
+        Returns: None.
+        """
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int or type(value) is float:
+            if size >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be a number")
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
