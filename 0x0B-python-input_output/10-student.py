87b2788a61f70cf6f910ff1d5c07b195abc97e32
Ossai <calebossaiakpe@gmail.com>
2023-04-13 10:08:15 -0700

    new commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module contains the Student class
+"""
+
+
+class Student:
+    """
+    A Student class that contains three attributes
+    and a method to_json
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        instantiating the object
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+     def to_json(self, attrs=None):
+        """
+        This method returns a dictionary representation
+        of the Student class
+        """
+        return self.__dict__
---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+"""
+import json
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, 'w', encoding='utf-8') as _file:
+         
