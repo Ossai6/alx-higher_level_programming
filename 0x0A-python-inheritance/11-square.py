c57c7cd1f9ea68d040918d6f31ff76fffbbcce09
Ossai <calebossaiakpe@gmail.com>
2023-04-13 10:18:10 -0700

    added 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module contains the class BaseGeometry and subclass Rectangle
+"""
+
+
+"""Importing the class Rectancle from 9-rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    This class has two methods
+    """
+    def __init__(self, size):
+        """instantiating the object"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """returns the area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        informal string reepresentation of the square
+        """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
