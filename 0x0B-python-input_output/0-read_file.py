79859e7439f7af656286ede62e19e3702bb34447
Ossai <calebossaiakpe@gmail.com>
2023-04-13 03:16:53 -0700

    firsr commit in python input and output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+This module.contains the read_file function
+"""
+
 
 def read_file(filename=""):
+    """
+    reading a text file (UTF8) and prints it to stdout
+    """
     with open(filename, "r", encoding="utf-8") as _file:
         print(_file.read())
