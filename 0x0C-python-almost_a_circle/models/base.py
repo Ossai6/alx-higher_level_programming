e92e9aec92b5eadbb218c0558870769bfb897b07
Ossai <calebossaiakpe@gmail.com>
2023-04-17 12:32:25 -0700

    added a readme file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,13 +32,21 @@ class Base(object):
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of
+        list_dictionaries:
+        """
         if list_dictionaries is None or list_dictionaries == "":
-            return  "[]"
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs
+        to a file:
+        """
         filename = cls.__name__ + ".json"
         with open(filename, 'w', encoding="utf-8") as _filename:
             if list_objs is None:
