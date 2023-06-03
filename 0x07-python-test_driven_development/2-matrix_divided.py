d083e3e642096b22f7ff37887dea7fbe6bfde3af
Ossai6 <calebossaiakpe@gmail.com>
2023-06-03 03:50:40 -0700

    updated

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,28 +1,32 @@
-#!/usr/bin/python3
-""". """
-
 def matrix_divided(matrix, div):
-    """. """
     if type(matrix) is list:
         for item in matrix:
-            if type(item) is list:
-                for elements in item:
-                    if type(elements) not in [int, float]:
-                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-                        first_row = matrix[0]
-                        for row in matrix:
-                            if len(row) != len(first_row):
-                                raise TypeError("Each row of the matrix must have the same size")
-                        if type(div) not in [int or float]:
-                                raise TypeError("div must be a number")
-                        if div == 0:
-                            raise ZeroDivisionError("division by zero")
-                        for item in matrix:
-                            for elements in item:
-                                new_matrix = elements/div
-                        return new_matrix
-
-            else:
+            if type(item) is not list:
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+            for element in item:
+                if type(element) not in [int, float]:
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+            if len(item) != len(matrix[0]):
+                raise TypeError("Each row of the matrix must have the same size")
+
+        if type(div) not in [int, float]:
+            raise TypeError("div must be a number")
+
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+
+        new_matrix = []
+        for row in matrix:
+            new_row = []
+            for element in row:
+                result = round((element / div), 2)
+                new_row.append(result)
+            new_matrix.append(new_row)
+
+        return new_matrix
+
     else:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
