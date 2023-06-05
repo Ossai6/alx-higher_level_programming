5b9a37a8db594ee8d4a85749dc236b2be5dbef96
Ossai6 <calebossaiakpe@gmail.com>
2023-06-05 05:13:21 -0700

    updated 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""This module contains the class Rectangle"""
+"""
+This module contains the class Rectangle
+"""
 
 
 class Rectangle():
     """This class represents a rectangle"""
     def __init__(self, width=0, height=0):
+        """initializes the rectangle"""
         self.height = height
         self.width = width
 
