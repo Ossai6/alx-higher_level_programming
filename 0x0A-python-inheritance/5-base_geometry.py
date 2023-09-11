3f76efac9b1ca04d1c9ead58b4ecaa216618232b
Ossai6 <calebossaiakpe@gmail.com>
2023-09-11 11:07:46 +0000

    added two new files

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This file contains a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ This is an empty class """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This file contains a class BaseGeometry """
+
+
+class BaseGeometry:
+    """ This is the BaseGeometry class """
+    def area(self):
+        """ raises an Exception with the message area() is not implemented """
+        raise Exception("area() is not implemented")
