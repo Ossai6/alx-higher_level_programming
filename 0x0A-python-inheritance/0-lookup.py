a943df297f9e069230d4e5795497ab4aa9ccbb20
Ossai6 <calebossaiakpe@gmail.com>
2023-09-11 09:39:16 +0000

    Added a readme and 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This file contains the def lookup(obj) function
+"""
+
+
+def lookup(obj):
+    """
+    This function returns the available attributes and methods of a list object
+    """
+    return(dir(list))
