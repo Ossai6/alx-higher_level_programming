3337779a3855bd4ff904bc198457af5abe15cfcd
Ossai6 <calebossaiakpe@gmail.com>
2023-09-15 15:44:07 +0000

    i added 0-read_file.py and readme file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This module contains read_file() function """
+
+
+def read_file(filename=""):
+    """
+    This function that reads a text file (UTF8)
+    and prints it to stdout
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())
