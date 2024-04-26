c08088150850a71004d005c1e832b2235ec4ec9c
Ossai6 <calebossaiakpe@gmail.com>
2024-04-26 07:58:09 +0000

    added a new directory for Python ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """  script that lists all states from the database hbtn_0e_0_usa """
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
+    db = MySQLdb.connect(port=3306, username=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3])
 
-    db = MySQLdb.connect(port=3306, username=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
-    
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states ORDER BY id")
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     states = cursor.fetchall()
 
     for state in states:
-        print(state)
+        print(list(state))
 
     cursor.close()
     db.close()
