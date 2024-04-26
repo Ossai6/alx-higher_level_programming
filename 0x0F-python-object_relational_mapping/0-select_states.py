f43b95752bab9f2cacb0b3682e965a4a370a5ffa
Ossai6 <calebossaiakpe@gmail.com>
2024-04-26 08:12:41 +0000

    added a new directory for Python ORM and made correctionss

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,13 +4,16 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(username=sys.argv[1],
-                         password=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, username=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3], charset="utf-8")
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id")
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 
     states = cursor.fetchall()
 
     [print(state) for state in states]
+
+    cursor.close()
+    db.close()
