892b5cb5460ee70ede4e80627f0c6ea75e9ae15b
Ossai6 <calebossaiakpe@gmail.com>
2024-04-26 07:59:41 +0000

    added a new directory for Python ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(port=3306, username=sys.argv[1],
+    db = MySQLdb.connect(username=sys.argv[1],
                          password=sys.argv[2], db=sys.argv[3])
 
     cursor = db.cursor()
