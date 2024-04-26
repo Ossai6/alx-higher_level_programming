4078cc166e340f816ab0a7c12f84d78b18f2753d
Ossai6 <calebossaiakpe@gmail.com>
2024-04-26 09:07:41 +0000

    fix the code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""  script that lists all states from the database hbtn_0e_0_usa """
 
-import sys
+"""
+This script lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+
+Usage: ./1-filter_states.py <mysql_username> <mysql_password> <database_name>
+"""
 import MySQLdb
+import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, username=sys.argv[1],
